ab07f4c7db85b19d1f3fe105ae195b3d36d87e9c
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2023-11-03 09:32:04 -0600

    first commit

--- ZebraLector/ViewController.swift
+++ ZebraLector/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
+    var m_ReaderList: [String] = []
+    var api = srfidSdkFactory.createRfidSdkApiInstance()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func startScanner(_ sender: Any) {
+        showMessageBox(message: "text", title: "hola")
+    }
+    
+    func showMessageBox(message: String, title: String) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let okButton = UIAlertAction(title: "Ok", style: .cancel)
+            alert.addAction(okButton)
+            self.present(alert, animated: true)
+        }
+    }
+    func startNFCReading() {
+        
+    }
+}
+
+extension ViewController: zt_IRfidAppEngineDevListDelegate {
+    func deviceListHasBeenUpdated() -> Bool {
+        return true
+    }
 
 }
 
+protocol zt_IRfidAppEngineDevListDelegate: AnyObject {
+    func deviceListHasBeenUpdated() -> Bool
+}
---
+++ ZebraLector/ZebraLector-Bridging-Header.h
@@ -0,0 +1,54 @@
+//
+//  ZebraLector-Bridging-Header.h
+//  ZebraLector
+//
+//  Created by Miguel Mexicano Herrera on 02/11/23.
+//
+
+#ifndef ZebraLector_Bridging_Header_h
+#define ZebraLector_Bridging_Header_h
+
+
+#endif /* ZebraLector_Bridging_Header_h */
+#import "RfidAttribute.h"
+#import "RfidSdkApiDelegate.h"
+#import "RfidSdkFactory.h"
+#import "RfidSdkDefs.h"
+#import "RfidSdkApiDelegate.h"
+#import "RfidReportConfig.h"
+#import "RfidAccessConfig.h"
+#import "RfidAntennaConfiguration.h"
+#import "RfidDynamicPowerConfig.h"
+#import "RfidSingulationConfig.h"
+#import "RfidTagReportConfig.h"
+#import "RfidReaderVersionInfo.h"
+#import "RfidReaderCapabilitiesInfo.h"
+#import "RfidStartTriggerConfig.h"
+#import "RfidStopTriggerConfig.h"
+#import "RfidRegulatoryConfig.h"
+#import "RfidPreFilter.h"
+#import "RfidAttribute.h"
+#import "RfidAccessCriteria.h"
+#import "RfidAccessParameters.h"
+#import "RfidUniqueTagsReport.h"
+#import "RfidUntraceableConfig.h"
+#import "ISbtSdkApiDelegate.h"
+#import "RfidSdkDefs.h"
+#import "RfidSdkApiDelegate.h"
+#import "RfidReportConfig.h"
+#import "RfidAccessConfig.h"
+#import "RfidAntennaConfiguration.h"
+#import "RfidDynamicPowerConfig.h"
+#import "RfidSingulationConfig.h"
+#import "RfidTagReportConfig.h"
+#import "RfidReaderVersionInfo.h"
+#import "RfidReaderCapabilitiesInfo.h"
+#import "RfidStartTriggerConfig.h"
+#import "RfidStopTriggerConfig.h"
+#import "RfidRegulatoryConfig.h"
+#import "RfidPreFilter.h"
+#import "RfidAttribute.h"
+#import "RfidAccessCriteria.h"
+#import "RfidAccessParameters.h"
+#import "RfidUniqueTagsReport.h"
+#import "RfidUntraceableConfig.h"
